8665ba8f7bab35c78cc8aa0b90152ffa630a5316
Hartrickb <Hartrickb@users.noreply.github.com>
2017-10-31 12:03:13 -0400

    add: added a loginRegister button

--- chatApp/Controller/LoginController.swift
+++ chatApp/Controller/LoginController.swift
@@ -18,15 +18,27 @@ class LoginController: UIViewController {
         view.layer.masksToBounds = true
         return view
     }()
-
+    
+    let loginRegisterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(r: 80, g: 101, b: 161)
+        button.setTitle("Register", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
         
         view.addSubview(inputsContainerView)
+        view.addSubview(loginRegisterButton)
         
         setupInputesContainerView()
+        setupLoginRegisterButton()
     }
     
     func setupInputesContainerView() {
@@ -35,6 +47,16 @@ class LoginController: UIViewController {
         inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
+    
+    func setupLoginRegisterButton() {
+        //need x, y, width, height constraints
+        
+        loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginRegisterButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 12).isActive = true
+        loginRegisterButton.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -48,4 +70,10 @@ extension UIColor {
         self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
     }
 }
+
+
+
+
+
+
 
