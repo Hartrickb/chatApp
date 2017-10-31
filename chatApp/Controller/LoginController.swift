e1c0e3f486e77d2b1772862d191aca85fd8b36f3
Hartrickb <Hartrickb@users.noreply.github.com>
2017-10-31 11:36:48 -0400

    create: Chat App using Firebase

---
+++ chatApp/Controller/LoginController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginController.swift
+//  chatApp
+//
+//  Created by Bennett Hartrick on 10/31/17.
+//  Copyright © 2017 Bennett. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+}
+
+extension UIColor {
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+}
+
---
+++ chatApp/Controller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  chatApp
+//
+//  Created by Bennett Hartrick on 10/31/17.
+//  Copyright © 2017 Bennett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        
+    }
+    
+    @objc func handleLogout() {
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
+    
+}
+
