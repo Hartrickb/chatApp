3efed99fff48efc824510713c3fe58069f628755
Hartrickb <Hartrickb@users.noreply.github.com>
2017-11-14 22:59:41 -0500

    create: creates the start of a bubbleView for the text boxes

--- chatApp/View/ChatMessageCell.swift
+++ chatApp/View/ChatMessageCell.swift
@@ -12,18 +12,34 @@ class ChatMessageCell: UICollectionViewCell {
     
     let textView: UITextView = {
         let tv = UITextView()
-        tv.text = "SAMPLE TEXT FOR NOW"
+//        tv.text = "SAMPLE TEXT FOR NOW"
         tv.font = UIFont.systemFont(ofSize: 16)
         tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.backgroundColor = UIColor.clear
+        tv.textColor = UIColor.white
         return tv
+    }()
+    
+    let bubbleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(r: 0, g: 137, b: 249)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        addSubview(bubbleView)
         addSubview(textView)
         
         // ios 9 constraints
+        // x, y, width, height
+        bubbleView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        bubbleView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        bubbleView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        bubbleView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+        
         // x, y, width, height
         textView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
         textView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
